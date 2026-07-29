43375829dc06d51f32aee82b121a0b9fba7dba15
viniciusbettoe <viniciusbetto@gmail.com>
2026-07-29 04:13:00 -0300

    Texto em Miúsculo

--- assets/js/util.js
+++ assets/js/util.js
@@ -49,3 +49,65 @@ function iconeDespesa(tipo){
     }
 }
 
+
+document.addEventListener('input', (event) => {
+    const elemento = event.target;
+    // Verifica se o elemento é um campo de texto (input ou textarea)
+    if (elemento.tagName === 'INPUT' || elemento.tagName === 'TEXTAREA') {
+        if (elemento.hasAttribute('data-maiusculo')) {
+            elemento.value = elemento.value.toUpperCase();
+        }
+        if (elemento.hasAttribute('data-minusculo')) {
+            elemento.value = elemento.value.toLowerCase();
+        }
+        // TELEFONE
+        if (elemento.hasAttribute('data-telefone')) {
+            elemento.value = mascaraTelefone(elemento.value);
+        }
+        // VALOR MONETARIO
+        if (elemento.hasAttribute('data-valor')) {
+            let casas =
+                elemento.getAttribute('data-valor');
+            // padrão = 2 casas
+            casas = casas ? parseInt(casas) : 2;
+            elemento.value =
+                mascaraValor(elemento.value, casas);
+        }
+    }
+});
+
+// ======================================================
+// MASCARA TELEFONE
+// ======================================================
+
+function mascaraTelefone(valor) {
+    if (!valor) return '';
+    valor = valor.replace(/\D/g, '');
+    // (11) 99999-9999
+    valor = valor.replace(
+        /(\d{2})(\d)/,
+        '($1) $2'
+    );
+    valor = valor.replace(
+        /(\d)(\d{4})$/,
+        '$1-$2'
+    );
+    return valor;
+}
+
+// ======================================================
+// MASCARA VALOR
+// ======================================================
+
+function mascaraValor(valor, decimais = 2) {
+    if (!valor) return '';
+    // somente numeros
+    valor = valor.replace(/\D/g, '');
+    valor = Number(valor);
+    valor = valor / Math.pow(10, decimais);
+    return valor.toLocaleString('pt-BR', {
+        minimumFractionDigits: decimais,
+        maximumFractionDigits: decimais
+    });
+}
+
--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Versão do cache
 // =====================================================
 
-const CACHE_NAME = "MotoTrip-v1.0.0";
+const CACHE_NAME = "MotoTrip-v1.1.0";
 // Arquivos essenciais
 const APP_ASSETS = [
     "./",
