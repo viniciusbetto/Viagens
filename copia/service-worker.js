bc2d4684d4a439bd3cfd10ae2e5927dad3b14d8c
viniciusbettoe <viniciusbetto@gmail.com>
2026-09-01 16:42:56 -0300

    GITIGNORE

--- copia/service-worker.js
+++ copia/service-worker.js
@@ -3,7 +3,7 @@
 // Versão do cache
 // =====================================================
 
-const CACHE_NAME = "MotoTrip-v1.1.0";
+const CACHE_NAME = "MotoTrip-v1.2.0";
 // Arquivos essenciais
 const APP_ASSETS = [
     "./",
